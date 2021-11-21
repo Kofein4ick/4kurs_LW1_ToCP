43fbedf0f7c0305ba3f4eec98bd9d255779b4eda
Kofein4ick <tog2sosiska@gmail.com>
2021-11-21 13:22:26 +0300

    Изменение work с class на struct

--- LW1_ToCP/Work.h
+++ LW1_ToCP/Work.h
@@ -1,23 +1,9 @@
 #include <string>
 
-class Work {
-private:
+struct Work {
 	std::string prev;
 	int std_time;
 	int min_time;
 	int std_cost;
 	int min_cost;
-public:
-	Work();
-	~Work();
-	void set_prev(std::string str);
-	std::string get_prev();
-	void set_std_time(int time);
-	int get_std_time();
-	void set_min_time(int time);
-	int get_min_time();
-	void set_std_cost(int cost);
-	int get_std_cost();
-	void set_min_cost(int cost);
-	int get_min_cost();
 };
--- LW1_ToCP/main.cpp
+++ LW1_ToCP/main.cpp
@@ -12,7 +12,6 @@ int main()
 	unsigned int** Matrix;
 	int c_rib=0;
 	int count = 0;
-	int j = 0;
 	std::string* str;
 	Work* cl_w;
 	//Читаем файл
@@ -21,17 +20,18 @@ int main()
 	cl_w = new Work[c_rib];
 	//Разбиваем строку, заносим в объект
 	for (int i = 0; i < c_rib; i++){
-		cl_w[i].set_prev(str_frag(str[i], j));
+		int j = 0;
+		cl_w[i].prev = str_frag(str[i], j);
 		j++;
-		cl_w[i].set_std_time(std::stoi(str_frag(str[i], j)));
+		cl_w[i].std_time = std::stoi(str_frag(str[i], j));
 		j++;
-		cl_w[i].set_min_time(std::stoi(str_frag(str[i], j)));
+		cl_w[i].min_time = std::stoi(str_frag(str[i], j));
 		j++;
-		cl_w[i].set_std_cost(std::stoi(str_frag(str[i], j)));
+		cl_w[i].std_cost = std::stoi(str_frag(str[i], j));
 		j++;
-		cl_w[i].set_min_cost(std::stoi(str_frag(str[i], j)));
-		j = 0;
+		cl_w[i].min_cost = std::stoi(str_frag(str[i], j));
 	}
+
 	//Рассчёт кол-ва узлов(подсчёт кол-ва строк без повторений+1(для последней дуги))
 	//MSize = count + 2;
 	//Создание самой сети(заполнение матрицы)
